2e6154f3743784215dc12fd28aba57aa1bf032b1
Brihanccy <vargasbrihanccy@gmail.com>
2017-06-26 17:57:43 -0600

    adelanto

---
+++ dic2.py
@@ -0,0 +1,48 @@
+import random
+
+NOMBRES = [
+    'Ana',
+    'Pedro',
+    'Pablo',
+    'Ernesto',
+    'Ariel',
+    'Carlos',
+    'Luis',
+    'Oscar',
+    'Alicia',
+    'Maria',
+    'Brenda'
+]
+
+CIUDADES = [
+    'Managua',
+    'Masaya',
+    'Matagalpa',
+    'Chinandega',
+    'Somoto',
+    'Rivas'
+]
+
+
+def generar_diccionario_estudiantes():
+    estudiantes = {}
+
+    for nombre in NOMBRES:
+        estudiantes[nombre] = {
+            'edad': random.randrange(16, 30),
+            'anio': random.randrange(1, 5),
+            'ciudad': random.choice(CIUDADES)
+        }
+
+    return estudiantes
+
+libreria=generar_diccionario_estudiantes()
+
+for clave,valor in libreria.iteritems():
+    lista= 'El nombre del estudiante es {0} de edad {1}, es de la ciudad de {2} y ' \
+           'cursa {3} en la universidad'
+    print (lista.format(clave,valor['edad'],valor['ciudad'],valor['anio']))
+for clave,valor in libreria .iteritems():
+    lista1 = 'el estudiante {0} de la ciudad de {1} es de edad {2}, es de la ciudad de {3}, y' \
+             'cursa {3} en la universidad'
+    print (lista1.format(clave,valor['ciudad'],valor['edad'],valor['anio']))
