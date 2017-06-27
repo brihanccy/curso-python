2e5aa13675370e69d164f16e013da264b52a1de6
vargasbrihanccy@gmail.com <udem@fedora.mshome.net>
2017-06-27 16:59:45 -0600

    este es  el modificado

--- dic2.py
+++ dic2.py
@@ -40,22 +40,25 @@ libreria=generar_diccionario_estudiantes()
 
 for clave,valor in libreria.iteritems():
     lista= 'El nombre del estudiante es {0} de edad {1}, es de la ciudad de {2} y ' \
-           'cursa {3} en la universidad'
+           'cursa {3} en la universidad \n'
     print (lista.format(clave,valor['edad'],valor['ciudad'],valor['anio']))
 for clave,valor in libreria .iteritems():
     lista1 = 'el estudiante {0} de la ciudad de {1} es de edad {2}, es de la ciudad de {3}, y' \
-             'cursa {3} en la universidad'
+             'cursa {3} en la universidad \n'
     if valor['ciudad'] =='Managua':
         print (lista1.format(clave,valor['ciudad'],valor['edad'],valor['anio']))
 for clave, valor in libreria.iteritems():
     lista2 = 'el estudiante {0} de la ciudad de {1} es de edad {2}, es de la ciudad de {3}, y' \
-             'cursa {3} en la universidad'
+             'cursa {3} en la universidad \n'
     if valor['ciudad'] == 'Masaya' and valor['anio']== 1:
         print (lista1.format(clave,valor['ciudad'],valor['edad'],valor['anio']))
+
 for clave, valor in libreria.iteritems():
     lista3 = 'el estudiante {0} de la ciudad de {1} es de edad {2}, es de la ciudad de {3}, y' \
-             'cursa {3} en la universidad'
+             'cursa {3} en la universidad \n'
     if valor ['edad'] < 21 :
         print (lista1.format(clave, valor['ciudad'], valor['edad'], valor['anio']))
-
+        f = open('dic2.txt', 'a')
+        f.write(lista1.format(clave, valor['ciudad'], valor['edad'], valor['anio']))
+        f.close()
 
